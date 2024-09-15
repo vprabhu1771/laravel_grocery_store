c5d9a0e952cea4f154bed810bf24923cd0bb5a6d
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2024-09-15 21:31:31 +0530

    Bootstrap, Jquery added

--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -6,6 +6,11 @@
     <title>
         @yield('title', 'Default Title')
     </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- Add this in your <head> section for Bootstrap Icons -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.0/font/bootstrap-icons.min.css" rel="stylesheet">
+
 </head>
 <body>
 
@@ -15,10 +20,15 @@
     <!-- Include the Topbar partial -->
     @include('frontend.layout.topbar')
 
+    <!-- Include the Search partial -->
+    <!-- @include('frontend.layout.search') -->
+
     @yield('content')
 
     <!-- Include the Footer partial -->
     @include('frontend.layout.footer')
 
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
